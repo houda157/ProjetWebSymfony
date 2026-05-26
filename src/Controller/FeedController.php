ef5e0c2311696c3b6254ca4a9744bfe4f59f0d55
May-Abdallah <may.abdallah@insat.ucar.tn>
2026-05-27 00:09:09 +0100

    add like/unlike event

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\EventRepository;
 use App\Repository\LikeRepository;
 use App\Repository\StudentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,16 +20,52 @@ class FeedController extends AbstractController
         LikeRepository $likeRepo,
         StudentRepository $studentRepo
     ): Response {
-        $events = $eventRepo->findBy([], ['eventDate' => 'DESC']);
-        $now    = new \DateTime();
+        $user    = $this->getUser();
+        $student = $user ? $studentRepo->findOneBy(['user' => $user]) : null;
+        $events  = $eventRepo->findBy([], ['eventDate' => 'DESC']);
+        $now     = new \DateTime();
 
         return $this->render('feed/index.html.twig', [
             'events'   => $events,
             'upcoming' => [],
-            'student'  => null,
+            'student'  => $student,
             'likeRepo' => $likeRepo,
             'now'      => $now,
         ]);
+    }
+
+    #[Route('/like/{id}', name: 'app_like', methods: ['POST'])]
+    public function like(
+        int $id,
+        EventRepository $eventRepo,
+        LikeRepository $likeRepo,
+        StudentRepository $studentRepo,
+        EntityManagerInterface $em
+    ): Response {
+        $user    = $this->getUser();
+        $student = $user ? $studentRepo->findOneBy(['user' => $user]) : null;
+        $event   = $eventRepo->find($id);
+
+        if (!$student || !$event) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $existingLike = $likeRepo->findOneBy([
+            'student' => $student,
+            'event'   => $event,
+        ]);
+
+        if ($existingLike) {
+            $em->remove($existingLike);
+        } else {
+            $like = new \App\Entity\Like();
+            $like->setStudent($student);
+            $like->setEvent($event);
+            $em->persist($like);
+        }
+
+        $em->flush();
+        return $this->redirectToRoute('app_home');
     }
 
     #[Route('/student/profile/{id}', name: 'student_profile')]
